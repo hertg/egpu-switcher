349f98053565ce4964e4c6898ec20515c54d91c5
Michael <3533648+hertg@users.noreply.github.com>
2022-09-17 23:17:57 +0200

    update gh actions

--- .ci/generate-pkgbuild.sh
+++ .ci/generate-pkgbuild.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# Parameters
-# ==========
-# $1: Version
-# $2: SHA256 Checksum
-
+version=$1
+checksum=$2
 
 #### egpu-switcher ####
 
@@ -14,11 +11,11 @@ cp ./.pkg/aur/egpu-switcher/* ./.pkgbuild/egpu-switcher
 cat << EOF > ./.pkgbuild/egpu-switcher/PKGBUILD
 # Maintainer: hertg <aur@her.tg>
 # This file is generated automatically
-_version=$1
-_versionWithoutPrefix=$(echo $1 | cut -c2-)
+_version=$version
+_versionWithoutPrefix=${version#v}
 _pkgname=egpu-switcher
-_pkgver=$(echo $1 | sed 's/\([^-]*-g\)/r\1/;s/-/./g')
-_source=\${_pkgname}-\${_version}::https://github.com/hertg/egpu-switcher/archive/refs/tags/$1.tar.gz
+_pkgver=$(echo $version | sed 's/\([^-]*-g\)/r\1/;s/-/./g')
+_source=\${_pkgname}-\${_version}::https://github.com/hertg/egpu-switcher/archive/refs/tags/$version.tar.gz
 EOF
 
 cat ./.pkgbuild/egpu-switcher/PKGBUILD.template >> ./.pkgbuild/egpu-switcher/PKGBUILD
@@ -33,11 +30,11 @@ cp ./.pkg/aur/egpu-switcher-bin/* ./.pkgbuild/egpu-switcher-bin
 cat << EOF > ./.pkgbuild/egpu-switcher-bin/PKGBUILD
 # Maintainer: hertg <aur@her.tg>
 # This file is generated automatically
-_version=$1
+_version=$version
 _pkgname=egpu-switcher-bin
-_pkgver=$(echo $1 | sed 's/\([^-]*-g\)/r\1/;s/-/./g')
-_sha256sum=$2
-_source=\${_pkgname}-\${_pkgver}::https://github.com/hertg/egpu-switcher/releases/download/$1/egpu-switcher-amd64
+_pkgver=$(echo $version | sed 's/\([^-]*-g\)/r\1/;s/-/./g')
+_sha256sum=$checksum
+_source=\${_pkgname}-\${_pkgver}::https://github.com/hertg/egpu-switcher/releases/download/$version/egpu-switcher-amd64
 EOF
 
 cat ./.pkgbuild/egpu-switcher-bin/PKGBUILD.template >> ./.pkgbuild/egpu-switcher-bin/PKGBUILD
