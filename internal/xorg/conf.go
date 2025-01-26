758ab7c292596baa1081a7ef6ca9283821394dbf
Michael Hertig <michael@hertig.dev>
2025-01-26 20:14:43 +0100

    add possibility to use custom x11 config template, #114, #42

--- internal/xorg/conf.go
+++ internal/xorg/conf.go
@@ -3,15 +3,18 @@ package xorg
 import (
 	"bytes"
 	_ "embed"
+	"errors"
 	"fmt"
-	"html/template"
+	"io"
 	"os"
+	"text/template"
 
 	"github.com/hertg/egpu-switcher/internal/logger"
 )
 
 //go:embed conf.template
-var confTemplate string
+var embeddedTemplate string
+var templatePath = "/usr/share/egpu-switcher/x11-template.conf"
 
 func RemoveEgpuFile(path string, verbose bool) error {
 	f, _ := os.Stat(path)
@@ -44,7 +47,7 @@ func CreateEgpuFile(path string, contents string, verbose bool) error {
 	return nil
 }
 
-func RenderConf(id string, driver string, busid string, modesetting bool) string {
+func RenderConf(id string, driver string, busid string, modesetting bool, verbose bool) (string, bool, error) {
 
 	type conf struct {
 		Id          string
@@ -61,11 +64,36 @@ func RenderConf(id string, driver string, busid string, modesetting bool) string
 	}
 
 	buf := bytes.NewBuffer(nil)
-	t := template.Must(template.New("conf").Parse(confTemplate))
-	err := t.Execute(buf, c)
+
+	var confTemplate string
+
+	templateFile, err := os.OpenFile(templatePath, os.O_RDONLY, 0644)
+	isCustom := false
 	if err != nil {
-		panic(err)
+		if !errors.Is(err, os.ErrNotExist) {
+			// if we get an error, other than "not exists", print an error
+			// the "not exists" error is an expected outcome when the config file is not customized
+			logger.Error("unable to open custom x11 config template, using default template instead")
+		}
+		if verbose {
+			logger.Debug("using default template for x11 conf")
+		}
+		confTemplate = embeddedTemplate
+	} else {
+		if verbose {
+			logger.Debug("using custom template at '%s' for x11 conf", templatePath)
+		}
+		var buf bytes.Buffer
+		io.Copy(&buf, templateFile)
+		confTemplate = buf.String()
+		isCustom = true
 	}
 
-	return buf.String()
+	t := template.Must(template.New("conf").Parse(confTemplate))
+	err = t.Execute(buf, c)
+	if err != nil {
+		return "", isCustom, err
+	}
+
+	return buf.String(), isCustom, nil
 }
